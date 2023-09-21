ad3c194e09584de2711f7fa09889cadf3fecf252
Flávio <flaviomouracar7800@gmail.com>
2023-09-21 13:56:45 -0300

    Implementado repository de Empresa.

--- server/src/repositories/EmpresaRepository.ts
+++ server/src/repositories/EmpresaRepository.ts
@@ -1,6 +1,13 @@
 import { Empresa } from "../entities/Empresa";
 import { BaseRepository } from "./BaseRepository";
+import { EnderecoRepository } from "./EnderecoRepository";
 
 export class EmpresaRepository extends BaseRepository<Empresa> {
-  constructor() { super("Empresa") }
+  constructor(private readonly enderecoRepository = new EnderecoRepository(),
+  ) { super("Empresa") }
+
+  async create(data: Empresa): Promise<Empresa> {
+      await this.enderecoRepository.create(data.endereco);
+      return this.repository.save(data);
+  }
 }
