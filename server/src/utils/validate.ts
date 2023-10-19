1ced61af032bad702f12abca18d741341b39709a
Guilherme-FCM <guilherme_fcm@hotmail.com>
2023-10-19 00:26:31 -0300

    Format validate data removing invalid params

--- server/src/utils/validate.ts
+++ server/src/utils/validate.ts
@@ -2,7 +2,7 @@ import { ValidationError, validateSync } from "class-validator";
 import { ValidationException } from "../exceptions/ValidationException";
 
 export function validate<T extends Object>(params: Object, type: { new (): T }) {
-  const data = Object.assign(new type(), params);
+  const data = plainToClass(params, type);
   const validationErrors = validateSync(data);
   
   if (validationErrors.length > 0) {
@@ -15,3 +15,15 @@ export function validate<T extends Object>(params: Object, type: { new (): T }) 
 const formatError = (error: ValidationError) => ({
   [error.property]: Object.values(error.constraints || [])
 });
+
+function plainToClass<T extends Object>(data: any, type: { new (): T }): T {
+  const keys = Object.keys(new type());
+  
+  const object: any = {};
+  keys.forEach(key => {
+    if (data.hasOwnProperty(key)) 
+      object[key] = data[key];
+  })
+
+  return Object.assign(new type(), object);
+}
