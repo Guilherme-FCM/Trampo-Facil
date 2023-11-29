c6d9d31cf2bc6bf312639ee06d8e0cc2371ca4a8
Guilherme-FCM <guilherme_fcm@hotmail.com>
2023-11-29 01:18:15 -0300

    Show experiences and jobs

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,15 +1,15 @@
 <template>
-  <TitleCard
-    :title="candidato?.nome_completo"
-  />
+  <TitleCard :title="usuario?.nome_completo || usuario?.razao_social" />
   <v-container>
     <v-row justify="space-between">
         <v-col cols="8" size style="font-size: 20px">
-          <div class="pb-3"><strong>CPF: </strong><span>{{ candidato?.cpf }}</span></div>
-          <div class="pb-3"><strong>E-mail: </strong><span>{{ candidato?.email }}</span></div>
-          <div class="pb-3"><strong>Sexo: </strong><span>{{ candidato?.sexo }}</span></div>
-          <div class="pb-3" v-if="candidato?.endereco?.descricao">
-            <strong>Endereço: </strong><span>{{ candidato?.endereco?.descricao }}</span></div>
+          <div v-if="usuario?.cnpj" class="pb-3"><strong>CNPJ: </strong><span>{{ usuario?.cnpj }}</span></div>
+          <div v-if="usuario?.cpf" class="pb-3"><strong>CPF: </strong><span>{{ usuario?.cpf }}</span></div>
+          <div class="pb-3"><strong>E-mail: </strong><span>{{ usuario?.email }}</span></div>
+          <div v-if="usuario?.sexo" class="pb-3"><strong>Sexo: </strong><span>{{ usuario?.sexo }}</span></div>
+          <div v-if="usuario?.area_atuacao" class="pb-3"><strong>Área de Atuação: </strong><span>{{ usuario?.area_atuacao }}</span></div>
+          <div class="pb-3" v-if="usuario?.endereco?.descricao">
+            <strong>Endereço: </strong><span>{{ usuario?.endereco?.descricao }}</span></div>
         </v-col>
         <v-col cols="3">
           <v-card>
@@ -19,15 +19,59 @@
     </v-row>
     <v-row>
       <v-col class="text-h5">
-        <div><strong>Minhas Experiências</strong></div>
+        <div v-if="usuario?.experiencias"><strong>Minhas Experiências</strong></div> 
+        <div v-if="usuario?.vagas"><strong>Minhas Vagas</strong></div> 
+      </v-col>
+      <v-col cols="auto">
+        <v-btn>Criar</v-btn>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col v-if="candidato?.experiencias?.length == 0">
+
+    <v-row v-if="usuario?.experiencias">
+      <v-col cols="12" v-if="usuario.experiencias.length == 0">
         <div>Nenhuma experiência inserida</div>
       </v-col>
-      <v-col cols="12" v-for="(experiencia, i) in candidato?.experiencias">
-        <div>{{ experiencia.id }}</div>
+      <v-col cols="12" v-else>
+        <v-table>
+          <thead>
+            <tr>
+              <th class="text-left" v-for="(title, i) in experienciaHeaders" :key="i">
+                {{ title }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, i) in usuario?.experiencias" :key="i">
+              <td>{{ item.cargo }}</td>
+              <td>{{ item.descricao }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+
+    <v-row v-if="usuario?.vagas">
+      <v-col cols="12" v-if="usuario.vagas.length == 0">
+        <div>Nenhuma vaga inserida</div>
+      </v-col>
+      <v-col cols="12" v-else>
+        <v-table>
+          <thead>
+            <tr>
+              <th class="text-left" v-for="(title, i) in vagaHeaders" :key="i">
+                {{ title }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, i) in usuario?.vagas" :key="i">
+              <td>{{ item.cargo }}</td>
+              <td>{{ item.remuneracao }}</td>
+              <td>{{ item.turno }}</td>
+              <td>{{ item.contrato }}</td>
+            </tr>
+          </tbody>
+        </v-table>
       </v-col>
     </v-row>
   </v-container>
@@ -39,21 +83,44 @@ import { useCandidatoStore } from "@/store/candidato.store";
 import { onMounted } from "vue";
 import semImg from '@/assets/sem_imagem.png';
 import { useLocalStorage } from "@/store/localStorage.store";
-import { computed } from "vue";
+import { useEmpresaStore } from "@/store/empresa.store";
+import { Candidato } from "@/types/Candidato";
+import { Empresa } from "@/types/Empresa";
+import { ref } from "vue";
 
 const CandidatoStore = useCandidatoStore()
+const EmpresaStore = useEmpresaStore()
 const LocalStorageStore = useLocalStorage()
 
-const candidato = computed(() => CandidatoStore.$state)
+const usuario = ref<Candidato | Empresa>()
 
-onMounted(() => {
+onMounted(async () => {
   const userId = LocalStorageStore.user?.id
-  if (!userId)
+  const userType = LocalStorageStore.user?.type
+
+  if (!userId || !userType)
     return router.push('/404')
 
-  CandidatoStore.getById(userId)
+  if (userType === 1) {
+    await CandidatoStore.getById(userId)
+    usuario.value = CandidatoStore.$state;
+  } else if (userType === 2) {
+    await EmpresaStore.getById(userId)
+    usuario.value = EmpresaStore.$state;
+  }
 });
 
+const vagaHeaders = [
+  'Cargo',
+  'Remuneração',
+  'Turno',
+  'Contrato',
+]
+
+const experienciaHeaders = [
+  'Cargo',
+  'Descrição',
+]
 </script>
 
 <style scoped>
--- server/src/repositories/EmpresaRepository.ts
+++ server/src/repositories/EmpresaRepository.ts
@@ -3,4 +3,10 @@ import { BaseRepository } from "./BaseRepository";
 
 export class EmpresaRepository extends BaseRepository<Empresa> {
   constructor() { super(Empresa) }
+
+  async findById(id: any): Promise<Empresa | null> {
+    const empresa = await this.repository.findOneBy({ id });
+    await empresa?.vagas;
+    return { ...empresa, vagas: empresa.__vagas__ };
+  }
 }
